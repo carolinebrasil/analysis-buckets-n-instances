312140b991905081bfd7f61804df7c604e00c479
Caroline Brasil <caroline.brasil@telefonica.com>
2021-05-10 20:59:18 -0300

    💡

--- data-analysis-ec2.py
+++ data-analysis-ec2.py
@@ -4,16 +4,18 @@ from table_logger import TableLogger # Lib to exhibition data in table
 ec2 = boto3.resource('ec2')
 
 # Table to organize and show the information 
-table = TableLogger(columns='Creation date, ID, Type,State, Private_IP', border=True)
+table = TableLogger(columns='ID, Type, Platform, State, Private_IP', border=True)
 
 for i in ec2.instances.all():
     instancedata = dict() 
     instancedata['ID'] = i.id
     instancedata['Type'] = i.instance_type
     instancedata['State'] = i.state['Name']
-    instancedata['Public_IP'] = i.private_ip_address
-    instancedata['Creation_date'] = i.launch_time
+    instancedata['Private_IP'] = i.private_ip_address
+    #instancedata['Creation_date'] = i.launch_time
+    instancedata['Platform'] = i.platform
+    #instancedata['OS2'] = i.InstancePlatform
     #instancedata['security_groups'] = i.security_groups
 
     # Show the information in table format
-    table(instancedata['Creation_date'],instancedata['ID'], instancedata['Type'], instancedata['State'],instancedata['Public_IP'])
+    table(instancedata['ID'], instancedata['Type'], instancedata['Platform'], instancedata['State'],instancedata['Private_IP'])
